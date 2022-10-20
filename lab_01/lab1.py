7f2bfc9d97858e71fe371f45d9ef454ef47d5c72
Basil Tkachenko <baziltkachenko@gmail.com>
2022-10-20 15:56:29 +0300

    refactor v.4

--- lab_01/lab1.py
+++ lab_01/lab1.py
@@ -5,29 +5,32 @@ import numpy as np
 
 from st_aggrid import AgGrid
 
-# Cache dataframe so it's only loaded once
-@st.experimental_memo
-def load_data():
-    return pd.DataFrame(
-        {
-            "x" : [0.00, 0.15, 0.30, 0.45, 0.60, 0.75, 0.90, 1.05],
-            "y" : [1.000000, 0.838771, 0.655336, 0.450447, 0.225336, -0.018310, -0.278390, -0.552430],
-            "y'" : [-1.000000, -1.14944, -1.29552, -1.43497, -1.56464, -1.68164, -1.78333, -1.86742]
-        }
-    )
-
-# Boolean to resize the dataframe, stired as a session state variable
-st.checkbox("Use container width", value=False, key="use_container_width")
-
-df = load_data()
-
-# Display the datdframe and allow the user to stretch the dataframe
-#accross the full width of the container, based on the chackbox value
-st.dataframe(df, use_container_width=st.session_state.use_container_width)
-
 def main():
     st.markdown("### Lab 1 - Engineering of polynomial interpolation of tabular functions")
     st.markdown("Purpose - Developing of skills in engineering og algorithms for interpolation of tabular functions by "
                 "the Newton "
                 "and Hermite polynomial")
+
+    x_arr = [0.00, 0.15, 0.30, 0.45, 0.60, 0.75, 0.90, 1.05]
+    y_arr = [1.000000, 0.838771, 0.655336, 0.450447, 0.225336, -0.018310, -0.278390, -0.552430]
+    y_div_arr = [-1.000000, -1.14944, -1.29552, -1.43497, -1.56464, -1.68164, -1.78333, -1.86742]
+
+    df = pd.DataFrame({
+        "x" : x_arr,
+        "y" : y_arr,
+        "y'" : y_div_arr
+    })
+
+
+    st.subheader("Table of the function with N nodes:")
+    grid_return = AgGrid(
+        df,
+        editable=True,
+        height=300,
+        reload_data=False,
+        theme="bright",
+    )
+    arr = grid_return["data"].to_numpy()
+
+
 
