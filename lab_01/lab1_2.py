a7ec38bf4737de4d1fd9a44e74b536b04f597fbe
Basil Tkachenko <baziltkachenko@gmail.com>
2022-10-31 14:56:49 +0300

    lab1_newton_9

---
+++ lab_01/lab1_2.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+from st_aggrid import AgGrid
+
+class HermitInterpolationPolynomial(object):
+    '''
+    Класс для интерполяции полинома Эрмита
+    -----
+    Передаваемые параметры
+
+
+    '''
