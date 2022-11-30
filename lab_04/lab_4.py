5a7570a84b18f9dd50675b7a23e08833b7792f56
Basil Tkachenko <baziltkachenko@gmail.com>
2022-11-30 10:44:20 +0300

    lab_4(v.1)

--- lab_04/lab_4.py
+++ lab_04/lab_4.py
@@ -26,3 +26,19 @@ def runge_derivative(n: int, x_arr_len: int, y_arr: List, h: int) -> float:
     return (-y_arr[n + 2] + 4 * y_arr[n + 1] - 3 * y_arr[n]) / (2 * h)
 
 
+def alignment_variables(n: int, x_arr_len: int, y_arr: List, x_arr: List) -> float:
+    '''Выравнивающие переменные'''
+    if n + 1 < x_arr_len:
+        return y_arr[n] / x_arr[n] * x_arr[n + 1] / y_arr[n + 1] * (y_arr[n] - y_arr[n + 1]) / (x_arr[n] - x_arr[n + 1])
+    return y_arr[n - 1] / x_arr[n - 1] * x_arr[n] / y_arr[n] * (y_arr[n - 1] - y_arr[n]) / (x_arr[n - 1] - x_arr[n])
+
+
+def second_derivative(n: int, x_arr_len: int, y_arr: List, h: int) -> float:
+    '''Вторая разностная производная'''
+    if n == 0:
+        return (y_arr[2] - 2 * y_arr[1] + y_arr[0]) / (h * h)
+    elif n + 1 < x_arr_len:
+        return (y_arr[n + 1] - 2 * y_arr[n] + y_arr[n - 1]) / (h * h)
+    return (y_arr[n] - 2 * y_arr[n - 1] + y_arr[n - 2]) / (h * h)
+
+
