e09e1f5e7cd9f2ddd238546183f68a87c8d33c92
Basil Tkachenko <baziltkachenko@gmail.com>
2022-12-06 18:20:15 +0300

    nir initial commit v.6

--- lab_02/lab2_3.py
+++ lab_02/lab2_3.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import random
+from st_aggrid import AgGrid
 
 def main():
     st.markdown("### Лабораторная работа №2")
@@ -12,41 +12,29 @@ def main():
     Получение навыков построения алгоритма реализации метода наименьших квадратов с использованием полиномов 
     заданной степени в одномерном и двумерном вариантах при аппроксимации табличных функций с весами""")
 
-    a1, a2, a3 = st.columns(3)
-    b1, b2, b3 = st.columns(3)
-    c1, c2, c3 = st.columns(3)
-    d1, d2, d3 = st.columns(3)
-    e1, e2, e3 = st.columns(3)
-    f1, f2, f3 = st.columns(3)
 
+    x_array = [2., 2.5, 3., 3.5, 4., 4.5, 5., 5.5, 6., 6.5, 7., 7.5, 8., 8.5, 9., 9.5, 10.]
+    y_array = [3.36022917, 1.38151692, 7.75382231, 1.47294493, 4.90192966, 2.65940507, 1.30867122, 6.20313785,
+               6.67092352, 8.47001833, 5.25194885, 11.75337077, 8.01389994, 11.24240111, 8.7774639, 8.93281672, 15.72259106]
 
-    x1 = a1.number_input("x₁", min_value=-10, max_value=10, value=2., step=1)
-    x2 = b1.number_input("x₂", min_value=-10, max_value=10, value=2.5, step=1)
-    x3 = c1.number_input("x₃", min_value=-10, max_value=10, value=3., step=1)
-    x4 = d1.number_input("x₄", min_value=-10, max_value=10, value=3.5, step=1)
-    x5 = e1.number_input("x₅", min_value=-10, max_value=10, value=4., step=1)
-    x6 = f1.number_input("x₆", min_value=-10, max_value=10, value=4.5, step=1)
-
-    y1 = a2.number_input("y₁:", min_value=-10, max_value=10., value=3.36022917, format="%.4f")
-    y2 = b2.number_input("y₂", min_value=-10, max_value=10., value=1.38151692, format="%.4f")
-    y3 = c2.number_input("y₃", min_value=-10, max_value=10., value=7.75382231, format="%.4f")
-    y4 = d2.number_input("y₄", min_value=-10, max_value=10., value=1.47294493, format="%.4f")
-    y5 = e2.number_input("y₅", min_value=-10, max_value=10., value=4.90192966, format="%.4f")
-    y6 = f2.number_input("y₆", min_value=-10, max_value=10., value=2.65940507, format="%.4f")
-
-    x_array = [x1, x2, x3, x4, x5, x6]
-    y_array = [y1, y2, y3, y4, y5, y6]
-    x_array_len = len(x_array)
-    x_array_range = range(x_array_len)
-
-    st.write("---")
-    st.write("Итоговая таблица:")
-    result_data = {
+    df = pd.DataFrame({
         "x": x_array,
         "y": y_array,
-        }
-    result = pd.DataFrame(data=result_data).applymap("{0:.4f}".format)
-    st.table(result.assign(hack="").set_index("hack"))
+    })
+
+    st.subheader("Таблица функции с количеством узлов N:")
+
+    grid_return_2 = AgGrid(
+        df,
+        editable=True,
+        height=300,
+        reload_data=False,
+        theme="alpine",
+    )
+
+
+
+
 
 if __name__=="__main__":
     main()
